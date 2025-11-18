0f828231b5d2ee67a5db07e0da1fc36798c086f3
Павлова Милана <milpavlowa@yandex.com>
2025-11-18 14:44:58 +0500

    преобразовала функцию

---
+++ js/creating-photos.js
@@ -0,0 +1,34 @@
+import { getRandomInteger, getRandomArrayElement } from './utils.js';
+import { NAMES, SURNAMES, DESCRIPTIONS, MESSAGES, PHOTO_COUNT } from './const.js';
+
+const createMessage = () => {
+  const message = [getRandomArrayElement(MESSAGES)];
+  if (Math.random() < 0.5) {
+    message.push(getRandomArrayElement(MESSAGES));
+  }
+  return message.join(' ');
+};
+
+let commentId = 1;
+const createComment = () => ({
+  id: commentId++,
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: createMessage(),
+  name: `${getRandomArrayElement(NAMES)} ${getRandomArrayElement(SURNAMES)}`,
+});
+
+const createPhoto = (id) => ({
+  id,
+  url: `photos/${id}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from(
+    { length: getRandomInteger(0, 30) },
+    createComment
+  ),
+});
+
+const photos = () => Array.from({ length: PHOTO_COUNT }, (_, i) =>
+  createPhoto(i + 1)
+);
+export { photos };
