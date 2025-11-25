2e2fa67adc5d18448cda403c0e042d4971936d16
Павлова Милана <milpavlowa@yandex.com>
2025-11-25 13:23:56 +0500

    открытие полноразмерного фото по клику на миниатюру

---
+++ js/pictures.js
@@ -0,0 +1,36 @@
+import { onPhotoMiniatureClick } from './big-picture-modal.js';
+
+const createThumbnailElement = (photoData) => {
+  const template = document.querySelector('#picture');
+  const thumbnail = template.content.querySelector('.picture').cloneNode(true);
+
+  const imgElement = thumbnail.querySelector('.picture__img');
+  imgElement.src = photoData.url;
+  imgElement.alt = photoData.description;
+
+  thumbnail.querySelector('.picture__likes').textContent = photoData.likes;
+  thumbnail.querySelector('.picture__comments').textContent = photoData.comments.length;
+
+  thumbnail.dataset.thumbnailId = photoData.id;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    onPhotoMiniatureClick(photoData);
+  });
+
+  return thumbnail;
+};
+
+const renderThumbnails = (photoList) => {
+  const picturesContainer = document.querySelector('.pictures');
+  const fragment = document.createDocumentFragment();
+
+  photoList.forEach((photo) => {
+    const thumbnailElement = createThumbnailElement(photo);
+    fragment.appendChild(thumbnailElement);
+  });
+
+  picturesContainer.appendChild(fragment);
+};
+
+export { renderThumbnails };
