ab7ff74ce096cc4106f8e72173bb5f67bebc40e6
Павлова Милана <milpavlowa@yandex.com>
2025-12-04 15:19:24 +0500

    Resolve all merge conflicts

--- js/utils.js
+++ js/utils.js
@@ -4,11 +4,14 @@ const getRandomInteger = (a, b) => {
   return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 };
 
-const getRandomArrayElement = (elements) =>
-  elements[getRandomInteger(0, elements.length - 1)];
+export const getRandomArrayElement = (array) =>
+  array[getRandomInt(0, array.length - 1)];
 
-export { getRandomInteger, getRandomArrayElement };
-<<<<<<< HEAD
-=======
+export const createIdGenerator = () => {
+  let lastGeneratedId = 0;
 
->>>>>>> 8edcd188e94aea1cf4013dba35ddc81c5d4bb908
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
