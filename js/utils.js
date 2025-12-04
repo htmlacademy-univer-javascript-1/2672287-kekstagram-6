55a21f8ac886433e6e74652e35b7339769bed034
Павлова Милана <milpavlowa@yandex.com>
2025-12-04 23:23:42 +0500

    выполнила задание

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,24 @@
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-export const getRandomArrayElement = (array) =>
-  array[getRandomInt(0, array.length - 1)];
+const getRandomArrayElement = (array) =>
+  array[getRandomInteger(0, array.length - 1)];
 
-export const createIdGenerator = () => {
-  let lastGeneratedId = 0;
-
-  return () => {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
+const getRandomNoRepeatInt = (min, max, usedArray) => {
+  let currentValue;
+  currentValue = getRandomInteger(min, max);
+  while (usedArray.includes(currentValue))
+  {
+    currentValue = getRandomInteger(min, max);
+  }
+  usedArray.push(currentValue);
+  return currentValue;
 };
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInteger, getRandomArrayElement, getRandomNoRepeatInt, isEscapeKey};
