166c8949f8e06735e71b8384b1d3a7c890b6c044
Павлова Милана <milpavlowa@yandex.com>
2025-12-09 22:45:24 +0500

    новый файл

---
+++ js/image-editing.js
@@ -0,0 +1,38 @@
+import { isEscapeKey } from './utils.js';
+import { resetFormValidation, initFormValidation } from './form-validation.js';
+
+const imageUploading = document.querySelector('.img-upload__input');
+const imageEditor = document.querySelector('.img-upload__overlay');
+const mainWindow = document.body;
+const closeEditor = document.querySelector('.img-upload__cancel');
+const form = document.querySelector('.img-upload__form');
+const hashtagField = form.querySelector('.text__hashtags');
+const commentField = form.querySelector('.text__description');
+
+const isTextFieldFocused = () => document.activeElement === hashtagField || document.activeElement === commentField;
+
+const closeImageEditor = () => {
+  form.reset();
+  imageUploading.value = '';
+  mainWindow.classList.remove('modal-open');
+  imageEditor.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  resetFormValidation();
+};
+
+const openImageEditor = () => {
+  imageEditor.classList.remove('hidden');
+  mainWindow.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+  initFormValidation();
+};
+
+function onDocumentKeydown (evt) {
+  if (isEscapeKey(evt) && !isTextFieldFocused()) {
+    evt.preventDefault();
+    closeImageEditor();
+  }
+}
+
+imageUploading.addEventListener('change', openImageEditor);
+closeEditor.addEventListener('click', closeImageEditor);
