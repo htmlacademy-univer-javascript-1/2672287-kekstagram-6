14045cd1c349913632ade52e1146d97d7bc49fcc
Павлова Милана <milpavlowa@yandex.com>
2025-12-14 14:38:03 +0500

    добавила импорты

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-import './pictures.js';
+import { renderPictures } from './pictures.js';
 import './big-picture-modal.js';
 import './image-editing.js';
 import './image-scale.js';
+import { getData } from './api.js';
+import { showErrorMessage } from './form-validation.js';
+
+getData()
+  .then((data) => {
+    renderPictures(data);
+  })
+  .catch((error) => {
+    showErrorMessage(error.message);
+  });
