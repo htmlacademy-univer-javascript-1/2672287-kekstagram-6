d138add7541b3756375edc47fbc677e8416f2c76
Павлова Милана <milpavlowa@yandex.com>
2025-12-19 18:10:34 +0500

    добавление фильтров для фотографий и исправление синтаксиса

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { renderPictures } from './pictures.js';
+import { initFilters } from './filters.js';
 import './big-picture-modal.js';
 import './image-editing.js';
 import './image-scale.js';
@@ -8,6 +9,7 @@ import { showErrorMessage } from './form-validation.js';
 getData()
   .then((data) => {
     renderPictures(data);
+    initFilters(data);
   })
   .catch((error) => {
     showErrorMessage(error.message);
