65fd4dcb34ae278992e3a7c8d7c6e2b713e4e1c7
Павлова Милана <milpavlowa@yandex.com>
2025-12-19 18:11:06 +0500

    доработка

--- js/utils.js
+++ js/utils.js
@@ -27,4 +27,23 @@ const checkForRepeats = (array) => {
   return uniqueSet.size !== lowerCaseArray.length;
 };
 
-export {getRandomInteger, getRandomArrayElement, getRandomNoRepeatInt, isEscapeKey, checkForRepeats };
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export {getRandomInteger, getRandomArrayElement, getRandomNoRepeatInt, isEscapeKey, checkForRepeats, debounce, throttle };
